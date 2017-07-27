5fee38a20adc65da7d43eacea35160f7eff64f7a
santanu mukherjee <santanu.mukherjee@tcg-digital.com>
2017-07-27 11:32:59 +0530

    New Jenkins Project added...

---
+++ RESTJenkinsExample/src/main/java/com/jenkins/rest/JSONService.java
@@ -0,0 +1,27 @@
+package com.jenkins.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.jenkins.Track;
+
+@Path("/jenkins")
+public class JSONService {
+
+	@GET
+	@Path("/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Track getTrackInJSON() {
+
+		Track track = new Track();
+		track.setTitle("Enter Sandman");
+		track.setSinger("Metallica");
+
+		return track;
+
+	}
+
+	
+}
